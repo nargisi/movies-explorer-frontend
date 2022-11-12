82d445bab837d84bc46060b6ea6aaf29cf687425
Nargisi <st-ana2005@yandex.ru>
2022-11-12 15:54:04 +0300

    Fix section Techs, small refactoring

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -6,12 +6,12 @@ const Techs = () => {
   return (
     <section className="techs__container">
       <Line>Технологии</Line>
-      <h2 className="techs__title">7 технологий</h2>
-      <p className="techs__paragraph">
-        На курсе веб-разработки мы освоили технологии, которые применили в
-        дипломном проекте.
-      </p>
       <div className="techs__box">
+        <h2 className="techs__title">7 технологий</h2>
+        <p className="techs__paragraph">
+          На курсе веб-разработки мы освоили технологии, которые применили в
+          дипломном проекте.
+        </p>
         <ul className="techs__container-list">
           <li className="techs__container-list__item">HTML</li>
           <li className="techs__container-list__item">CSS</li>
